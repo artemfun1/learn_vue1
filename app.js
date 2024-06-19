492779e9c83ecc5e31b0208d9670e781093e2e35
Артем <artem@MacBook-Pro-Artem.local>
2024-06-19 14:27:52 +0500

    l

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
 const App = {
-	data() {
-		return {
-			title: "Список заметок",
-			placeholderString: "Введите название заметки",
-			inputValue: "",
-			notes: ["заметка 1", "заметка 2"],
-		};
-	},
+	data: ()=> ({
+		title:'я есть Грут'
+	}),
 	methods: {
-		addNewNote() {
-			if (this.inputValue !== "") {
-				this.notes.push(this.inputValue);
-				this.inputValue = "";
-			}
-		},
-		inputKeyPress() {
-			this.addNewNote();
-		},
-		toUpperCase(item) {
-			return item.toUpperCase();
-		},
-		removeNote(idx) {
-			this.notes.splice(idx, 1);
-		},
+		
 	},
   computed:{
-    doubleCountComputed(){
-      // console.log('doubleComputed')
-      return this.notes.length * 2
-    },
+    
   },
   watch:{
-    inputValue(value){
-      console.log(value, 'eee ')
-    }
+   
   }
 };
-
-
-
-
 
 
 
