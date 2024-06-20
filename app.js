5effd696b7db40275d57dee213cffe5a19595722
artemfun1 <a.o.peshkov@yandex.ru>
2024-06-20 15:50:57 +0500

    test1

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const App = {
 	data: ()=> ({
 		title:'я есть Грут',
-		myHtml: '<h1>Vue 3</h1>'
+		myHtml: '<h1>Vue 3</h1>',
+		person:{
+			firstName:'Test',
+			lastName: 'front',
+			age: 25
+
+		},
+		array: [1,2,3,4,5,6]
 	}),
 	methods: {
+		// stopPropagation(e){
+		// 	e.stopPropagation()
+		// }
 		
 	},
   computed:{
